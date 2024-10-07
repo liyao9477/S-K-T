adb0bf64c4e6326489b546b826f6d3bb8cd277ed
liyao9477 <60308713+liyao9477@users.noreply.github.com>
2024-10-07 11:51:16 +0800

    a

--- logic_R&SVM.py
+++ logic_R&SVM.py
@@ -31,23 +31,47 @@ plt.show()
 log_reg.predict([[1.71],[1.43]])
 
 
+# SVM
+# Linear SVM
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
 
+X = iris["data"][:,(2,3)]  
+y = (iris["target"] == 2).astype(np.int64)
 
+svm_clf = Pipeline([
+    ("scaler", StandardScaler()),
+    ("linear_svc", LinearSVC(C=1, loss="hinge"))
+    ])
+svm_clf.fit(X, y)
 
+svm_clf.predict([[4.71,1.63]])
 
+svm_clf.predict([[5.71,1.63]])
 
+# Nonlinear SVM
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
 
+X, y = make_moons(n_samples=100, noise=0.15)
+polynomial_svm_clf = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=3)),
+    ("scaler", StandardScaler()),
+    ("svm_clf", LinearSVC(C=10, loss="hinge"))
+    ])
+polynomial_svm_clf.fit(X, y)
 
+polynomial_svm_clf.predict([[4.71,1.63]])
 
+polynomial_svm_clf.predict([[5.71,1.63]])
 
+# SVM regresiong --> SVR
+from sklearn.svm import LinearSVR
 
-
-
-
-
-
-
-
+svm_reg = LinearSVR(epsilon=1.5)
+svm_reg.fit(X, y)
 
 
 
